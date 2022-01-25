174856a237d81e5796cfac01cd6095916f116779
Juho Kilpikoski <juho@speechly.com>
2022-01-25 09:40:10 +0200

    Add utterances cmd to fetch utterances (#32)
    * Add utterances cmd to fetch utterances
    
    * Fix typo

---
+++ cmd/utterances.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	analyticsv1 "github.com/speechly/api/go/speechly/analytics/v1"
+	"github.com/spf13/cobra"
+
+	"github.com/speechly/cli/pkg/clients"
+)
+
+var utterancesCmd = &cobra.Command{
+	Use:   "utterances <app_id>",
+	Short: "Get a sample of recent utterances.",
+	Long:  `Fetches a sample of recent utterances and their SAL-annotated transcript.`,
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx := cmd.Context()
+		appId := args[0]
+
+		client, err := clients.AnalyticsClient(ctx)
+		if err != nil {
+			log.Fatalf("Failed to init analytics client%s", err)
+		}
+		response, err := client.Utterances(ctx, &analyticsv1.UtterancesRequest{AppId: appId})
+		if err != nil {
+			log.Fatalf("Failed to fetch utterances data for %s: %s", appId, err)
+		}
+		for _, utt := range response.Utterances {
+			fmt.Printf("%s\t%s\t%s\n", utt.Date, utt.Annotated, utt.Transcript)
+		}
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(utterancesCmd)
+}
